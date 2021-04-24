583dbd4bee3fe40dc9d9c5084d68e99f64dbf732
abdelatief <abdelatief99@gmail.com>
2021-04-24 22:39:43 +0200

    create video controller

--- app/Controllers/Http/VideosController.ts
+++ app/Controllers/Http/VideosController.ts
@@ -1,6 +1,7 @@
 import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext"
 import Video from "App/Models/Video"
 import { rules, schema } from "@ioc:Adonis/Core/Validator"
+import VideoCreateValidator from "App/Validators/VideoCreateValidator"
 
 export default class VideosController {
     public async index({ response }: HttpContextContract) {
@@ -9,19 +10,29 @@ export default class VideosController {
     }
 
     public async store({ request, response }: HttpContextContract) {
-        const storeSchema = schema.create({
-            url: schema.string({}, [rules.url()]),
-            lesson_id: schema.number(),
-        })
+        const { url, lesson_id } = await request.validate(new VideoCreateValidator())
 
-        const { url, lesson_id } = await request.validate({ schema: storeSchema })
         const newVideo = await Video.create({ url, lesson_id })
         response.status(201).json({ new_video: newVideo.toJSON() })
     }
 
-    public async show({}: HttpContextContract) {}
+    public async show({ params, response }: HttpContextContract) {
+        const id = params.id
+        const video = await Video.query().where("id", id).firstOrFail()
+        response.status(200).json({ video: video.toJSON() })
+    }
 
-    public async update({}: HttpContextContract) {}
+    public async update({ params, request, response }: HttpContextContract) {
+        const id = params.id
+        const { url, lesson_id } = await request.validate(new VideoCreateValidator()) // video create and update has the same validation schema
+        const video = await Video.query().where("id", id).firstOrFail()
+        video.url = url
+        video.lesson_id = lesson_id
+        response.status(200).json({ updated_video: video.toJSON() })
+    }
 
-    public async destroy({}: HttpContextContract) {}
+    public async destroy({params, response}: HttpContextContract) {
+        await Video.query().where('id', params.id).delete()
+        response.status(200).json({ message: `video with id: ${params.id} is deleted successfully`})
+    }
 }
